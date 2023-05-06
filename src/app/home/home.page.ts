4eed1b4eefd18d8b9dd89f004e1f0161ae2138aa
Paulo Vitor <101908012+plvtrx@users.noreply.github.com>
2023-05-06 01:49:21 +0000

    comit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Produto, ProdutoService } from '../services/produto.service';
+import { LocalizacaoServices } from '../services/localizacao.service';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +11,26 @@ export class HomePage implements OnInit {
 
   public listaProdutos: Produto[] = [];
 
-  constructor(private produtoService: ProdutoService) { }
+  constructor(private produtoService: ProdutoService, private localizacaoService: LocalizacaoServices) { }
 
   ngOnInit() {
-    this.produtoService.getProdutos().subscribe(
-      (produtos) => {
-        this.listaProdutos = produtos;
-      }
-    )
+    setInterval(()=> {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position);
+        this.localizacaoService.inserir({
+          idusuario: 3,
+          longitude: position.coords.longitude,
+          latitude: position.coords.longitude,
+          horario: new Date(),
+          nome: "De frente com de costa"
+        }).subscribe();
+      }) 
+    }, 2000);
+    // this.produtoService.getProdutos().subscribe(
+    //   (produtos) => {
+    //     this.listaProdutos = produtos;
+    //   }
+    // )
   }
 
 }
